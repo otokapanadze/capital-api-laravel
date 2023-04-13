b0a7fc976f7b5e4ad1eeac938459676aa23b2725
Oto Kapanadze <otokapanadze@icloud.com>
2023-04-14 03:24:18 +0400

    Added client request methods with guzzle

--- src/Http/ApiClient.php
+++ src/Http/ApiClient.php
@@ -3,27 +3,70 @@
 namespace OtoKapanadze\CapitalApi\Http;
 
 use OtoKapanadze\CapitalApi\Contracts\ApiClientInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class ApiClient implements ApiClientInterface
 {
+    protected $httpClient;
+
+    public function __construct()
+    {
+        $this->httpClient = new Client();
+    }
 
     public function get(string $endpoint, array $queryParams = []): array
     {
-        // TODO: Implement get() method.
+        try {
+            $response = $this->httpClient->get($endpoint, [
+                'query' => $queryParams,
+            ]);
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            throw new \RuntimeException('Error executing GET request: ' . $e->getMessage());
+        }
     }
 
-    public function post(string $endpoint, array $data): array
+    public function post(string $endpoint, array $data, array $headers = []): array
     {
-        // TODO: Implement post() method.
+        try {
+            $response = $this->httpClient->post($endpoint, [
+                'json' => $data,
+                'headers' => $headers,
+            ]);
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            throw new \RuntimeException('Error executing POST request: ' . $e->getMessage());
+        }
     }
 
-    public function put(string $endpoint, array $data): array
+    public function put(string $endpoint, array $data, array $headers = []): array
     {
-        // TODO: Implement put() method.
+        try {
+            $response = $this->httpClient->put($endpoint, [
+                'json' => $data,
+                'headers' => $headers,
+            ]);
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            throw new \RuntimeException('Error executing PUT request: ' . $e->getMessage());
+        }
     }
 
-    public function delete(string $endpoint, array $data = []): array
+    public function delete(string $endpoint, array $data = [], array $headers = []): array
     {
-        // TODO: Implement delete() method.
+        try {
+            $response = $this->httpClient->delete($endpoint, [
+                'json' => $data,
+                'headers' => $headers,
+            ]);
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            throw new \RuntimeException('Error executing DELETE request: ' . $e->getMessage());
+        }
     }
 }
